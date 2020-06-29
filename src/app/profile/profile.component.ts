b8a5964ac19d252d018c4ac926d0e502b6263372
DK <don.klanakin@gmail.com>
2020-06-30 02:49:10 +0700

    Added the profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
+  faCog = faCog;
   constructor() { }
 
   ngOnInit(): void {
