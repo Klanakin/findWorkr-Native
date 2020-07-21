687004958c5ec3bdbb2fd97b839fef534400a60e
DK <don.klanakin@hotmail.com>
2020-07-21 21:24:34 +0700

    Added the back button on the Graphics & Design page

--- src/app/graphic-design/graphic-design.component.ts
+++ src/app/graphic-design/graphic-design.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import {
   faCog,
-  faAngleRight } from '@fortawesome/free-solid-svg-icons';
+  faAngleRight,
+  faArrowLeft
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-graphic-design',
@@ -12,6 +14,7 @@ import {
 export class GraphicDesignComponent implements OnInit {
   faCog = faCog;
   faAngleRight = faAngleRight;
+  faArrowLeft = faArrowLeft;
 
   constructor() { }
 
