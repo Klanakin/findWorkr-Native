fe19a622c7f0211ce35646ac30a7cc1b45b535e1
DK <don.klanakin@hotmail.com>
2020-07-24 13:50:46 +0700

    Added the content sizing function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,11 +8,30 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-faSearch = faSearch;
-  
-  
+  faSearch = faSearch;
+  wholeContentSection1 = false;
+  wholeContentSection2 = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  seeAll(sectionId: string): void {
+    if (sectionId == 'section-1') {
+      this.wholeContentSection1 = true;
+    }
+    else if (sectionId == 'section-2') {
+      this.wholeContentSection2 = true;
+    }
+  }
+
+  seeLess(sectionId: string): void {
+    if (sectionId == 'section-1') {
+      this.wholeContentSection1 = false;
+    }
+    else if (sectionId == 'section-2') {
+      this.wholeContentSection2 = false;
+    }
   }
 }
